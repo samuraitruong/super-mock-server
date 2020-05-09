39354fcc4d10924771b98afa02c91b957aff0fb1
Truong Nguyen <tnguyen@demystdata.com>
2020-05-09 16:43:24 +1000

    tag the image with hashcommit too

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
+set -e
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker tag mockserver:latest samuraitruong/mockserver:latest
+docker tag mockserver:latest samuraitruong/mockserver:$IMAGE_TAG
 docker push samuraitruong/mockserver:latest
+docker push samuraitruong/mockserver:$IMAGE_TAG
